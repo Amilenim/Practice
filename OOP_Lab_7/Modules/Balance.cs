31f960d44305f10f332081fdbfa3b07810b682eb
Amil Bakhshaliev <a.e.bakhshaliiev@student.khai.edu>
2023-12-17 22:47:05 +0200

    Balance

---
+++ OOP_Lab_7/Modules/Balance.cs
@@ -0,0 +1,35 @@
+﻿namespace Shop.Modules
+{
+    public class ShopBalance
+    {
+        public int BalanceAdd(int cash)
+        {
+            try
+            {
+                int balance = 0;
+
+                Console.WriteLine($"\nНа балансi: {cash} грн");
+                Console.WriteLine("Додайте бажану сумму");
+                balance += Convert.ToInt32(Console.ReadLine());
+                if (balance > 0 && balance <= 1000)
+                    Console.WriteLine($"\nНа балансi: {cash + balance} грн");
+                else throw new Exception("\nВведiть число бiльше за 0 грн, та не бiльше за 1000 грн");
+                return balance;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\nВведiть тiльки цифри!");
+            }
+            return 0;
+        }
+        public int BalanceAdd(int cash, int random)
+        {
+            int balance = 0;
+
+            balance += random;
+            Console.WriteLine($"\nВiтаэмо! Ваш виграш складає {random} грн!");
+            Console.WriteLine($"На балансi: {cash + balance} грн");
+            return balance;
+        }
+    }
+}
