58ec29c815e2de26faea830f243140150d1128d4
Amil Bakhshaliev <a.e.bakhshaliiev@student.khai.edu>
2023-12-17 22:48:35 +0200

    Components

---
+++ OOP_Lab_7/Modules/Components.cs
@@ -0,0 +1,27 @@
+﻿using Shop.Enums;
+
+namespace Shop.Modules
+{
+    internal class Components
+    {
+        private List<int> vegetables = new List<int>() { 10, 20, 5, 15 };
+        private List<int> fruit = new List<int>() { 25, 70, 50, 40 };
+
+        public void Models()
+        {
+            Console.WriteLine("Рiзновид продуктiв:");
+            Console.WriteLine("\nОвочi\n");
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine($"{(Vegetables)i} {vegetables[i - 1]} grn");
+            }
+
+            Console.WriteLine("\nФрукти\n");
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine($"{(Fruits)i} {fruit[i - 1]} grn");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
